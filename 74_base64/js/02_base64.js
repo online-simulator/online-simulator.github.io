2425f36d316407c134c3ea75cc86a1efc8c022cf
online-simulator <fm2019a@yahoo.co.jp>
2020-12-26 06:18:36 +0900

    74

--- 74_base64/js/02_base64.js
+++ 74_base64/js/02_base64.js
@@ -3,6 +3,7 @@
 function My_str2base64(){
   var input = My$_id("input").value;
   var output = "";
+  var output_native = "";
   try{
     var binary = My_conv.str2binary(input);
     output = btoa(binary);
@@ -11,10 +12,18 @@ function My_str2base64(){
     output = e.message;
   }
   My$_id("output").value = output;
+  try{
+    output_native = btoa(input);
+  }
+  catch(e){
+    output_native = e.message;
+  }
+  My$_id("output_native").value = output_native;
 }
 function My_base64_2str(){
   var input = My$_id("input").value;
   var output = "";
+  var output_native = "";
   try{
     var binary = atob(input);
     output = My_conv.binary2str(binary);
@@ -23,11 +32,22 @@ function My_base64_2str(){
     output = e.message;
   }
   My$_id("output").value = output;
+  try{
+    output_native = atob(input);
+  }
+  catch(e){
+    output_native = e.message;
+  }
+  My$_id("output_native").value = output_native;
 }
 function My_postset(){
   My$_id("input").value = My$_id("output").value;
 }
+function My_postset_native(){
+  My$_id("input").value = My$_id("output_native").value;
+}
 function My_clear(){
   My$_id("input").value = "";
   My$_id("output").value = "";
+  My$_id("output_native").value = "";
 }
