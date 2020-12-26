7e444310106dd0583dc2b654750f674ec4e6c45b
online-simulator <fm2019a@yahoo.co.jp>
2020-12-26 14:26:28 +0900

    70

--- 00_common/js/01_driver/02_handler_dom.js
+++ 00_common/js/01_driver/02_handler_dom.js
@@ -8,14 +8,15 @@ My_handler_dom.prototype.init = function(){
   var self = this;
   return self;
 };
-My_handler_dom.prototype.init_drag = function(id_drag, id_checkbox, opt_handler){
+My_handler_dom.prototype.__drag__ = {};
+My_handler_dom.prototype.__drag__.init = function(id_drag, id_checkbox, opt_handler){
   var self = this;
   self.handler = opt_handler;
   self.drag = new My_drag(id_drag, opt_handler);
   self.checkbox = My$_id(id_checkbox);
   return self;
 };
-My_handler_dom.prototype.drag_checkbox = function(opt_handler){
+My_handler_dom.prototype.__drag__.switch = function(opt_handler){
   var self = this;
   if(self.checkbox.checked){
     self.drag.add_handler(opt_handler || self.handler);
