cbb3dc251cd4d872cc15fe58f169559dc792ec59
online-simulator <fm2019a@yahoo.co.jp>
2021-01-03 02:02:35 +0900

    70

--- 70_decimal/js/02_decimal.js
+++ 70_decimal/js/02_decimal.js
@@ -2,25 +2,25 @@
 
 function My_run_n2dec(){
   var val_n = My$_id("input-n").value;
-  var n = My$select_id("select-n2dec").textContent;
+  var n = My$selectNum_id("select-n2dec");
   var dec = My_conv.n2dec(val_n, n);
   My$_id("output-dec").value = dec;
-  var isChecked = My$_id("checkbox-dec-dec").checked;
+  var isChecked = My$checkbox_id("checkbox-dec-dec");
   if(isChecked){
     My$_id("input-dec").value = dec;
-    var n = My$select_id("select-dec2n").textContent;
+    var n = My$selectNum_id("select-dec2n");
     My$_id("output-n").value = My_conv.dec2n(dec, n);
   }
 }
 
 function My_run_dec2n(){
   var val_dec = My$_id("input-dec").value;
-  var n = My$select_id("select-dec2n").textContent;
+  var n = My$selectNum_id("select-dec2n");
   My$_id("output-n").value = My_conv.dec2n(val_dec, n);
-  var isChecked = My$_id("checkbox-dec-dec").checked;
+  var isChecked = My$checkbox_id("checkbox-dec-dec");
   if(isChecked){
     My$_id("output-dec").value = val_dec;
-    var n = My$select_id("select-n2dec").textContent;
+    var n = My$selectNum_id("select-n2dec");
     My$_id("input-n").value = My_conv.dec2n(val_dec, n);
   }
 }
