ba511ad7a81d94216e5d6417a197030835800bd2
online-simulator <fm2019a@yahoo.co.jp>
2021-01-04 01:22:37 +0900

    50

--- 50_link/js/02_link.js
+++ 50_link/js/02_link.js
@@ -5,7 +5,7 @@ const My_counter = {n: 0};
 const My_create_link = function(id_output, isInsertBefore){
   var n = My_counter.n++;
   var name = My$_id("input-name").value+n+".txt";
-  var json = {p: {id: "wrapper-link"}, a: {id: "a"+n, it: "download"+n}, name: name, type: "text/plain", isIB: isInsertBefore};
+  var json = {p: {id: "wrapper-link"}, a: {id: "a"+n, it: "download by double-click"+n}, name: name, type: "text/plain", isIB: isInsertBefore};
   json.o = (id_output)? {id: id_output}: {tag: "input"};
   var handler = new My_handler_link(json);
   return handler;
