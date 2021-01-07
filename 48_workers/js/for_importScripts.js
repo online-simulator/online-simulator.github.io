08b16a51124bde2d5f6f1319f95f9a249d29156d
online-simulator <fm2019a@yahoo.co.jp>
2021-01-08 01:00:49 +0900

    48

--- 48_workers/js/for_importScripts.js
+++ 48_workers/js/for_importScripts.js
@@ -1,17 +1,17 @@
 // online-simulator.github.io
 
-var My_job_imported = function(data){
+var My_job_imported = function(_data){
   var out = null;
   var isWorker = (typeof window === "undefined");
   try{
 //    error();
-    out = Math[data.sw_job](data.in);
+    out = Math[_data.sw_job](_data.in);
   }
   catch(e){
     out = e.message;
     out += (isWorker)? "@worker": "@main";
     throw new Error(out);
   }
-  data.out = out;
-  return data;
+  _data.out = out;
+  return _data;
 };
