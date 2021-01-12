16e7103644f71dd3740f886ddea282b9aa253034
online-simulator <fm2019a@yahoo.co.jp>
2021-01-12 15:42:14 +0900

    72

---
+++ 72_unicode/js/03_unicode.js
@@ -0,0 +1,120 @@
+// online-simulator.github.io
+
+function My_test_unicode(){
+  var self = this;
+  self.init.apply(self, arguments);
+  return self;
+}
+
+My_def.mix_in(My_test_unicode, My_original_main);
+
+My_test_unicode.prototype.init = function(){
+  var self = this;
+  self.init_main.apply(self, arguments);
+  return self;
+};
+My_test_unicode.prototype.init_elems = function(){
+  var self = this;
+  self.setup_elems_readonly(["textarea"]);
+  self.setup_elems(My$arr_tag("button"), "onclick");
+  return self;
+};
+My_test_unicode.prototype.init_handlers = function(){
+  var self = this;
+  self.handlers.onload = function(args){
+    var self = this;
+    return self;
+  };
+  self.handlers.onbeforeunload = function(e){
+    var self = this;
+    return self;
+  };
+  self.handlers.onclick = function(e, elem){
+    var self = this;
+    switch(elem.id){
+      case "clear":
+      case "str2code":
+      case "code2str":
+      case "postset_utf16":
+      case "postset_utf8":
+        self[elem.id]();
+        break;
+      default:
+        break;
+    }
+    return self;
+  };
+  self.handlers.onchange = function(e, elem){
+    var self = this;
+    switch(elem.id){
+      default:
+        break;
+    }
+    return self;
+  };
+  return self;
+};
+My_test_unicode.prototype.clear = function(){
+  var self = this;
+  My$_id("textarea-input").value = "";
+  My$_id("textarea-output_utf16").value = "";
+  My$_id("textarea-output_utf8").value = "";
+  return self;
+};
+My_test_unicode.prototype.str2code = function(){
+  var self = this;
+  var n = My$selectNum_id("select-n");
+  var input = My$_id("textarea-input").value;
+  var output_utf16 = "";
+  var output_utf8 = "";
+  try{
+    output_utf16 = My_conv.str2code_utf16(input, n);
+  }
+  catch(e){
+    output_utf16 = e.message;
+  }
+  My$_id("textarea-output_utf16").value = output_utf16;
+  try{
+    output_utf8 = My_conv.str2code_utf8(input, n);
+  }
+  catch(e){
+    output_utf8 = e.message;
+  }
+  My$_id("textarea-output_utf8").value = output_utf8;
+  return self;
+};
+My_test_unicode.prototype.code2str = function(){
+  var self = this;
+  var n = My$selectNum_id("select-n");
+  var input = My$_id("textarea-input").value;
+  var output_utf16 = "";
+  var output_utf8 = "";
+  var arr_num_n = input.split(",");
+  try{
+    var arr_uint16 = My_conv.arr_num2arr_uint(arr_num_n, n, 16);
+    output_utf16 = My_conv.arr_uint16_2str(arr_uint16);
+  }
+  catch(e){
+    output_utf16 = e.message;
+  }
+  My$_id("textarea-output_utf16").value = output_utf16;
+  try{
+    var arr_uint8 = My_conv.arr_num2arr_uint(arr_num_n, n, 8);
+    output_utf8 = My_conv.arr_uint8_2str(arr_uint8);
+  }
+  catch(e){
+    output_utf8 = e.message;
+  }
+  My$_id("textarea-output_utf8").value = output_utf8;
+  return self;
+};
+My_test_unicode.prototype.postset_utf16 = function(){
+  var self = this;
+  My$_id("textarea-input").value = My$_id("textarea-output_utf16").value;
+  return self;
+};
+My_test_unicode.prototype.postset_utf8 = function(){
+  var self = this;
+  My$_id("textarea-input").value = My$_id("textarea-output_utf8").value;
+  return self;
+};
