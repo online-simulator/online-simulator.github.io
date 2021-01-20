c2210676b3e8201074a7b999608583b7a240b1d4
online-simulator <fm2019a@yahoo.co.jp>
2021-01-20 22:29:32 +0900

    74

--- 74_base64/js/03_base64.js
+++ 74_base64/js/03_base64.js
@@ -1,26 +1,26 @@
 // online-simulator.github.io
 
-function My_test_base64(){
+My_entry.test_base64 = function(){
   var self = this;
   self.init.apply(self, arguments);
   return self;
-}
+};
 
-My_def.mix_in(My_test_base64, My_original_main);
+My_entry.def.mix_in(My_entry.test_base64, My_entry.original_main);
 
-My_test_base64.prototype.init = function(){
+My_entry.test_base64.prototype.init = function(){
   var self = this;
-  self.init_main.apply(self, arguments);
+  self.init_main.call(self, ["$", "conv"]);
   return self;
 };
-My_test_base64.prototype.init_elems = function(){
+My_entry.test_base64.prototype.init_elems = function(){
   var self = this;
-  My$.setup_elems_readonly$("textarea");
-  My$.setup_elems$_tag("button", self.handlers, "onclick");
+  self.entry.$.setup_elems_readonly$("textarea");
+  self.entry.$.setup_elems$_tag("button", self.handlers, "onclick");
   self.arr_sw_out = ["BE", "LE", "native"];
   return self;
 };
-My_test_base64.prototype.init_handlers = function(){
+My_entry.test_base64.prototype.init_handlers = function(){
   var self = this;
   self.handlers.onload = function(args){
     var self = this;
@@ -58,24 +58,24 @@ My_test_base64.prototype.init_handlers = function(){
   };
   return self;
 };
-My_test_base64.prototype.clear = function(){
+My_entry.test_base64.prototype.clear = function(){
   var self = this;
-  My$.arr("textarea").forEach(function(elem){
+  self.entry.$.arr("textarea").forEach(function(elem){
     elem.value = "";
   });
   return self;
 };
-My_test_base64.prototype.str2base64_sw = function(sw){
+My_entry.test_base64.prototype.str2base64_sw = function(sw){
   var self = this;
-  var input = My$._id("textarea-input").value;
+  var input = self.entry.$._id("textarea-input").value;
   var output = "";
   try{
     switch(sw){
       case "BE":
-        input = My_conv.str2binary(input);
+        input = self.entry.conv.str2binary(input);
         break;
       case "LE":
-        input = My_conv.str2binary(input, true);
+        input = self.entry.conv.str2binary(input, true);
         break;
       default:
         break;
@@ -85,28 +85,28 @@ My_test_base64.prototype.str2base64_sw = function(sw){
   catch(e){
     output = e.message;
   }
-  My$._id("textarea-output_"+sw).value = output;
+  self.entry.$._id("textarea-output_"+sw).value = output;
   return self;
 };
-My_test_base64.prototype.str2base64 = function(){
+My_entry.test_base64.prototype.str2base64 = function(){
   var self = this;
   self.arr_sw_out.forEach(function(sw){
     self.str2base64_sw(sw);
   });
   return self;
 };
-My_test_base64.prototype.base64_2str_sw = function(sw){
+My_entry.test_base64.prototype.base64_2str_sw = function(sw){
   var self = this;
-  var input = My$._id("textarea-input").value;
+  var input = self.entry.$._id("textarea-input").value;
   var output = "";
   try{
     output = atob(input);
     switch(sw){
       case "BE":
-        output = My_conv.binary2str(output);
+        output = self.entry.conv.binary2str(output);
         break;
       case "LE":
-        output = My_conv.binary2str(output, true);
+        output = self.entry.conv.binary2str(output, true);
         break;
       default:
         break;
@@ -115,18 +115,18 @@ My_test_base64.prototype.base64_2str_sw = function(sw){
   catch(e){
     output = e.message;
   }
-  My$._id("textarea-output_"+sw).value = output;
+  self.entry.$._id("textarea-output_"+sw).value = output;
   return self;
 };
-My_test_base64.prototype.base64_2str = function(){
+My_entry.test_base64.prototype.base64_2str = function(){
   var self = this;
   self.arr_sw_out.forEach(function(sw){
     self.base64_2str_sw(sw);
   });
   return self;
 };
-My_test_base64.prototype.postset_sw = function(sw){
+My_entry.test_base64.prototype.postset_sw = function(sw){
   var self = this;
-  My$._id("textarea-input").value = My$._id("textarea-output_"+sw).value;
+  self.entry.$._id("textarea-input").value = self.entry.$._id("textarea-output_"+sw).value;
   return self;
 };
