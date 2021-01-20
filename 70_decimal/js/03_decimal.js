47797a78ce51b461e61362f90a8515159b647ebe
online-simulator <fm2019a@yahoo.co.jp>
2021-01-20 22:28:51 +0900

    70

--- 70_decimal/js/03_decimal.js
+++ 70_decimal/js/03_decimal.js
@@ -1,34 +1,34 @@
 // online-simulator.github.io
 
-function My_test_decimal(){
+My_entry.test_decimal = function(){
   var self = this;
   self.init.apply(self, arguments);
   return self;
-}
+};
 
-My_def.mix_in(My_test_decimal, My_original_main);
+My_entry.def.mix_in(My_entry.test_decimal, My_entry.original_main);
 
-My_test_decimal.prototype.init = function(){
+My_entry.test_decimal.prototype.init = function(){
   var self = this;
-  self.init_main.apply(self, arguments);
+  self.init_main.call(self, ["$", "conv"]);
   return self;
 };
-My_test_decimal.prototype.init_elems = function(){
+My_entry.test_decimal.prototype.init_elems = function(){
   var self = this;
-  My$.setup_elems_readonly$("input");
-  My$.setup_elems$_tag("input", self.handlers, "onclick");
-  My$.setup_elems$_tag("input", self.handlers, "onchange");
-  My$.setup_elems$_tag("select", self.handlers, "onchange");
+  self.entry.$.setup_elems_readonly$("input");
+  self.entry.$.setup_elems$_tag("input", self.handlers, "onclick");
+  self.entry.$.setup_elems$_tag("input", self.handlers, "onchange");
+  self.entry.$.setup_elems$_tag("select", self.handlers, "onchange");
   return self;
 };
-My_test_decimal.prototype.init_handlers = function(){
+My_entry.test_decimal.prototype.init_handlers = function(){
   var self = this;
   self.handlers.onload = function(args){
     var self = this;
-    self.drag = new My_handler_drag("div-drag", "checkbox-drag", {});
-    My$._id("checkbox-drag").onchange();
-    My$._id("input-n").onchange();
-    My$._id("input-dec").onchange();
+    self.drag = new self.constructors.handler_drag("div-drag", "checkbox-drag", {});
+    self.entry.$._id("checkbox-drag").onchange();
+    self.entry.$._id("input-n").onchange();
+    self.entry.$._id("input-dec").onchange();
     return self;
   };
   self.handlers.onbeforeunload = function(e){
@@ -70,30 +70,30 @@ My_test_decimal.prototype.init_handlers = function(){
   };
   return self;
 };
-My_test_decimal.prototype.n2dec = function(){
+My_entry.test_decimal.prototype.n2dec = function(){
   var self = this;
-  var val_n = My$._id("input-n").value;
-  var n = My$.selectNum_id("select-n2dec");
-  var dec = My_conv.n2dec(val_n, n);
-  My$._id("output-dec").value = dec;
-  var isChecked = My$.checkbox_id("checkbox-dec-dec");
+  var val_n = self.entry.$._id("input-n").value;
+  var n = self.entry.$.selectNum_id("select-n2dec");
+  var dec = self.entry.conv.n2dec(val_n, n);
+  self.entry.$._id("output-dec").value = dec;
+  var isChecked = self.entry.$.checkbox_id("checkbox-dec-dec");
   if(isChecked){
-    My$._id("input-dec").value = dec;
-    var n = My$.selectNum_id("select-dec2n");
-    My$._id("output-n").value = My_conv.dec2n(dec, n);
+    self.entry.$._id("input-dec").value = dec;
+    var n = self.entry.$.selectNum_id("select-dec2n");
+    self.entry.$._id("output-n").value = self.entry.conv.dec2n(dec, n);
   }
   return self;
 };
-My_test_decimal.prototype.dec2n = function(){
+My_entry.test_decimal.prototype.dec2n = function(){
   var self = this;
-  var val_dec = My$._id("input-dec").value;
-  var n = My$.selectNum_id("select-dec2n");
-  My$._id("output-n").value = My_conv.dec2n(val_dec, n);
-  var isChecked = My$.checkbox_id("checkbox-dec-dec");
+  var val_dec = self.entry.$._id("input-dec").value;
+  var n = self.entry.$.selectNum_id("select-dec2n");
+  self.entry.$._id("output-n").value = self.entry.conv.dec2n(val_dec, n);
+  var isChecked = self.entry.$.checkbox_id("checkbox-dec-dec");
   if(isChecked){
-    My$._id("output-dec").value = val_dec;
-    var n = My$.selectNum_id("select-n2dec");
-    My$._id("input-n").value = My_conv.dec2n(val_dec, n);
+    self.entry.$._id("output-dec").value = val_dec;
+    var n = self.entry.$.selectNum_id("select-n2dec");
+    self.entry.$._id("input-n").value = self.entry.conv.dec2n(val_dec, n);
   }
   return self;
 };
