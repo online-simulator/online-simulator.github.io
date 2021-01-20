90aad096c05cf53fd32ab8b57c252f1afe731f2e
online-simulator <fm2019a@yahoo.co.jp>
2021-01-20 22:28:41 +0900

    50

--- 50_link/js/03_link.js
+++ 50_link/js/03_link.js
@@ -1,26 +1,26 @@
 // online-simulator.github.io
 
-function My_test_link(){
+My_entry.test_link = function(){
   var self = this;
   self.init.apply(self, arguments);
   return self;
-}
+};
 
-My_def.mix_in(My_test_link, My_original_main);
+My_entry.def.mix_in(My_entry.test_link, My_entry.original_main);
 
-My_test_link.prototype.init = function(){
+My_entry.test_link.prototype.init = function(){
   var self = this;
+  self.init_main.call(self, ["$"]);
   self.counter = 0;
   self.handler_link = null;
-  self.init_main.apply(self, arguments);
   return self;
 };
-My_test_link.prototype.init_elems = function(){
+My_entry.test_link.prototype.init_elems = function(){
   var self = this;
-  My$.setup_elems$_tag("button", self.handlers, "onclick");
+  self.entry.$.setup_elems$_tag("button", self.handlers, "onclick");
   return self;
 };
-My_test_link.prototype.init_handlers = function(){
+My_entry.test_link.prototype.init_handlers = function(){
   var self = this;
   self.handlers.onload = function(args){
     var self = this;
@@ -51,12 +51,12 @@ My_test_link.prototype.init_handlers = function(){
   };
   return self;
 };
-My_test_link.prototype.create = function(id_output, isInsertBefore){
+My_entry.test_link.prototype.create = function(id_output, isInsertBefore){
   var self = this;
   var n = self.counter++;
-  var name = My$._id("input-name").value+n;
+  var name = self.entry.$._id("input-name").value+n;
   var json = {p: {id: "wrapper-link"}, a: {id: "a"+n, it: "download-txt by double-click"+n}, name: name, ext: "txt", isIB: isInsertBefore};
   json.o = (id_output)? {id: id_output}: {tag: "input"};
-  self.handler_link = new My_handler_link(json);
+  self.handler_link = new self.constructors.handler_link(json);
   return self;
 };
