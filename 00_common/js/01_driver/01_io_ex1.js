b4f325b87106b106a71f6658961d7e02f6af7ad5
online-simulator <fm2019a@yahoo.co.jp>
2021-03-21 09:58:13 +0900

    entry 2.14.5.2.6+

--- 00_common/js/01_driver/01_io_ex1.js
+++ 00_common/js/01_driver/01_io_ex1.js
@@ -24,10 +24,10 @@ My_entry.io_ex1.prototype.get_selection_caret = function(elem){
   _ip = Math.min(len, _ip);
   return {l: _ip, r: _ip};
 };
-My_entry.io_ex1.prototype.get_selection_deleted = function(text, ips, isDEL){
+My_entry.io_ex1.prototype.get_selection_deleted = function(text, ips, opt_isDEL){
   var self = this;
   var _ips = {l: ips.l, r: ips.r};
-  if(isDEL){
+  if(opt_isDEL){
     _ips.r = (_ips.l < text.length)? _ips.l+1: _ips.l;
   }
   else{
@@ -35,21 +35,35 @@ My_entry.io_ex1.prototype.get_selection_deleted = function(text, ips, isDEL){
   }
   return _ips;
 };
-My_entry.io_ex1.prototype.insert_sw = function(elem, _text_, isDEL){
+My_entry.io_ex1.prototype.set_selection = function(elem, ip){
   var self = this;
   if(elem){
-    elem.focus();
+    if(self.entry.def.isDef(elem.setSelectionRange)){
+      var abs_ip = Math.max(0, ip);
+      elem.setSelectionRange(abs_ip, abs_ip);
+    }
+    elem.focus();  // last
+  }
+  return self;
+};
+My_entry.io_ex1.prototype.insert_sw = function(elem, _text_, opt_isDEL, opt_ip_offset){
+  var self = this;
+  if(elem){
+    elem.focus();  // first
     var sw_isDEL = (_text_ === null);
     var _text_ = _text_ || "";
     var text = self.read_text(elem);
     var ips = self.get_selection_caret(elem);
-    ips = (sw_isDEL)? self.get_selection_deleted(text, ips, isDEL): ips;
+    if(sw_isDEL){
+      ips = self.get_selection_deleted(text, ips, opt_isDEL);
+    }
     var newText = text.substr(0, ips.l)+_text_+text.substr(ips.r);
     self.write_text(elem, newText);
-    if(self.entry.def.isDef(elem.setSelectionRange)){
-      var ip = ips.l+_text_.length;
-      elem.setSelectionRange(ip, ip);
+    var ip = ips.l+_text_.length;
+    if(opt_ip_offset){
+      ip += opt_ip_offset;
     }
+    self.set_selection(elem, ip);
   }
   return self;
 };
@@ -71,8 +85,18 @@ My_entry.io_ex1.prototype.onclick_ans = function(elems){
   self.insert_sw(elems.i, text);
   return self;
 };
-My_entry.io_ex1.prototype.onclick_default = function(elems, text){
+My_entry.io_ex1.prototype["onclick_"+"<<"] = function(elems){
   var self = this;
-  self.insert_sw(elems.i, text);
+  self.set_selection(elems.i, 0);
+  return self;
+};
+My_entry.io_ex1.prototype["onclick_"+">>"] = function(elems){
+  var self = this;
+  self.set_selection(elems.i, self.read_text(elems.i).length);
+  return self;
+};
+My_entry.io_ex1.prototype.onclick_default = function(elems, text, opt_ip_offset){
+  var self = this;
+  self.insert_sw(elems.i, text, null, opt_ip_offset);
   return self;
 };
