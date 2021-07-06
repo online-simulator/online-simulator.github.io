9af177ddfdad9fbca5955cf7e6fa741e83a09c12
online-simulator <fm2019a@yahoo.co.jp>
2021-07-06 12:45:01 +0900

    entry 3.51.17.6.20

--- 00_common/js/000_namespace/000_entry.js
+++ 00_common/js/000_namespace/000_entry.js
@@ -5,13 +5,13 @@
 // ECMAScript6
 const My_entry = {
   // major update . minor update . minor change . fatal bug-fix . minor bug-fix
-  VERSION: "online-simulator.github.io.3.51.17.5.20",
+  VERSION: "online-simulator.github.io.3.51.17.6.20",
   // major update. minor update/change. fatal/minor bug-fix
   Ver: {
     // inner
-    plot: "1.0.1",
+    plot: "1.0.2",
     // outer
-    calc: "2.40.19",
+    calc: "2.40.20",
     edit: "",
     wave: "1.10.2",
     else: "0.18.4"
