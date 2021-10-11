a85cf17454feacdea0df4debff28cb7ae0f34ac2
online-simulator <fm2019a@yahoo.co.jp>
2021-10-11 09:08:45 +0900

    entry 4.68.22.9.24

--- 00_common/js/000_namespace/000_entry.js
+++ 00_common/js/000_namespace/000_entry.js
@@ -5,14 +5,14 @@
 // ECMAScript6
 const My_entry = {
   // major update . minor update . minor change . fatal bug-fix . minor bug-fix
-  VERSION: "online-simulator.github.io.4.68.21.9.24",
+  VERSION: "online-simulator.github.io.4.68.22.9.24",
   // major update. minor update/change. fatal/minor bug-fix
   Ver: {
     // inner
     plot: "1.11.6",
     // outer
     checklist: "1.0.0",
-    calc: "2.57.26",
+    calc: "2.58.26",
     circuit: "TBD-status",
     fluid: "TBD-status",
     cell: "TBD-status",
