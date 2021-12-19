3c4c28be463d55099695cadaa326814f660ab54b
online-simulator <fm2019a@yahoo.co.jp>
2021-12-19 18:23:42 +0900

    entry 4.102.39.12.28

--- 00_common/js/000_namespace/000_entry.js
+++ 00_common/js/000_namespace/000_entry.js
@@ -5,14 +5,14 @@
 // ECMAScript6
 const My_entry = {
   // major update . minor update . minor change . fatal bug-fix . minor bug-fix
-  VERSION: "online-simulator.github.io.4.102.38.12.28",
+  VERSION: "online-simulator.github.io.4.102.39.12.28",
   // major update. minor update/change. fatal/minor bug-fix
   Ver: {
     // inner
     plot: "1.22.7",
     // outer
     checklist: "1.0.0",
-    calc: "2.100.32",
+    calc: "2.101.32",
     circuit: "TBD-status",
     fluid: "TBD-status",
     cell: "TBD-status",
