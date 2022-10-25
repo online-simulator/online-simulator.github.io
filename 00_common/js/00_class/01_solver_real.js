fa8a611db5ee94961096595500a2b3dd62a1ef4f
online-simulator <76011671+online-simulator@users.noreply.github.com>
2022-10-25 10:12:54 +0900

    entry 5.151.67.12.39

---
+++ 00_common/js/00_class/01_solver_real.js
@@ -0,0 +1,252 @@
+// online-simulator.github.io
+
+My_entry.solver_real = function(){
+  var self = this;
+  self.init.apply(self, arguments);
+  return self;
+};
+
+My_entry.solver_real.prototype.init = function(){
+  var self = this;
+  new My_entry.original_main().setup_constructors.call(self);
+  new My_entry.original_main().make_instances.call(self, ["math"]);
+  return self;
+};
+My_entry.solver_real.prototype.gaussian = function(options, obj_Axb){
+  var self = this;
+  obj_Axb.N = obj_Axb.A.length;
+  self.gaussian_pre(options, obj_Axb);
+  self.gaussian_forward(options, obj_Axb);
+  self.gaussian_backward(options, obj_Axb);
+  return self;
+};
+My_entry.solver_real.prototype.gaussian_pre = function(options, obj_Axb){
+  var self = this;
+  var math = self.entry.math;
+  var N = obj_Axb.N;
+  var A = obj_Axb.A;
+  var b = obj_Axb.b;
+  var abs_pivot = 0;
+  var abs_non_pivot = 0;
+  var i_switch = 0;
+  for(var i=0; i<N-1; ++i){
+    i_switch = i;
+    abs_pivot = Math.abs(A[i][i]);
+    for(var ii=i+1; ii<N; ++ii){
+      abs_non_pivot = Math.abs(A[ii][i]);
+      if(abs_pivot < abs_non_pivot){
+        abs_pivot = abs_non_pivot;
+        i_switch = ii;
+      }
+    }
+    if(i !== i_switch){
+      math.switch_arr(A, i, i_switch);
+      math.switch_arr(b, i, i_switch);
+    }
+  }
+  return self;
+};
+My_entry.solver_real.prototype.gaussian_forward = function(options, obj_Axb){
+  var self = this;
+  var N = obj_Axb.N;
+  var A = obj_Axb.A;
+  var b = obj_Axb.b;
+  for(var i=0; i<N-1; ++i){
+    for(var ii=i+1; ii<N; ++ii){
+      var w = A[ii][i]/A[i][i];
+      A[ii][i] = 0;
+      for(j=i+1; j<N; ++j){
+        A[ii][j] -= A[i][j]*w;
+      }
+      b[ii] -= b[i]*w;
+    }
+  }
+  return self;
+};
+My_entry.solver_real.prototype.gaussian_backward = function(options, obj_Axb){
+  var self = this;
+  var N = obj_Axb.N;
+  var A = obj_Axb.A;
+  var x = obj_Axb.x;
+  var b = obj_Axb.b;
+  if(N > 0){
+    var i = N-1;
+    var nume = b[i];
+    var deno = A[i][i];
+    x[i] = nume/deno;
+  }
+  if(N > 1){
+    for(var i=N-2; i>=0; --i){
+      var nume = b[i];
+      for(var j=i+1; j<N; ++j){
+        nume -= A[i][j]*x[j];
+      }
+      var deno = A[i][i];
+      x[i] = nume/deno;
+    }
+  }
+  return self;
+};
+My_entry.solver_real.prototype.gaussian_coo = function(options, obj_Axb){
+  var self = this;
+  obj_Axb.N = obj_Axb.b.length;
+  try{
+    self.gaussian_coo_init(options, obj_Axb);
+    self.gaussian_coo_pre(options, obj_Axb);
+    self.gaussian_coo_forward(options, obj_Axb);
+    self.gaussian_coo_backward(options, obj_Axb);
+  }
+  catch(e){
+    throw "Invalid gauss_coo2mat";
+  }
+  return self;
+};
+My_entry.solver_real.prototype.gaussian_coo_init = function(options, obj_Axb){
+  var self = this;
+  var aA = obj_Axb.aA;
+  var mA = obj_Axb.mA;
+  var nA = obj_Axb.nA;
+  var m2arr_n = [];
+  var m2A = [];
+  var len_j = aA.length;
+  for(var j=0; j<len_j; ++j){
+    var m = mA[j];
+    var n = nA[j];
+    m2arr_n[m] = m2arr_n[m] || [];
+    m2arr_n[m].push(n);
+    m2A[m] = m2A[m] || [];
+    m2A[m].push(aA[j]);
+  }
+  obj_Axb.m2arr_n = m2arr_n;
+  obj_Axb.m2A = m2A;
+  return self;
+};
+My_entry.solver_real.prototype.get_jn = function(arr_n, n0, opt_isChecked){
+  var self = this;
+  var _jn = -1;
+  if(!(arr_n)) throw false;
+  var len_j = arr_n.length;
+  for(var j=0; j<len_j; ++j){
+    var n = arr_n[j];
+    if(n === n0){
+      _jn = j;
+      break;
+    }
+  }
+  if(opt_isChecked && j === len_j) throw false;
+  return _jn;
+};
+My_entry.solver_real.prototype.gaussian_coo_pre = function(options, obj_Axb){
+  var self = this;
+  var math = self.entry.math;
+  var N = obj_Axb.N;
+  var m2arr_n = obj_Axb.m2arr_n;
+  var m2A = obj_Axb.m2A;
+  var b = obj_Axb.b;
+  var abs_pivot = 0;
+  var abs_non_pivot = 0;
+  var i_switch = 0;
+  for(var i=0; i<N-1; ++i){
+    var m0 = i+1;
+    var n0 = i+1;
+    i_switch = i;
+    var aA0 = m2A[m0];
+    var jn0 = self.get_jn(m2arr_n[m0], n0);
+    var num_pivot = (jn0 === -1)? 0: aA0[jn0];  // pivot=0 allowed
+    abs_pivot = Math.abs(num_pivot);
+    for(var ii=i+1; ii<N; ++ii){
+      var m = ii+1;
+      var jn = self.get_jn(m2arr_n[m], n0);
+      if(jn !== -1){
+        var aA = m2A[m];
+        var num_non_pivot = aA[jn];
+        abs_non_pivot = Math.abs(num_non_pivot);
+        if(abs_pivot < abs_non_pivot){
+          abs_pivot = abs_non_pivot;
+          i_switch = ii;
+        }
+      }
+    }
+    if(i !== i_switch){
+      var m_switch = i_switch+1;
+      math.switch_arr(m2arr_n, m0, m_switch);
+      math.switch_arr(m2A, m0, m_switch);
+      math.switch_arr(b, i, i_switch);
+    }
+  }
+  return self;
+};
+My_entry.solver_real.prototype.gaussian_coo_forward = function(options, obj_Axb){
+  var self = this;
+  var N = obj_Axb.N;
+  var m2arr_n = obj_Axb.m2arr_n;
+  var m2A = obj_Axb.m2A;
+  var b = obj_Axb.b;
+  for(var i=0; i<N-1; ++i){
+    var m0 = i+1;
+    var n0 = i+1;
+    var aA0 = m2A[m0];
+    var num_pivot = aA0[self.get_jn(m2arr_n[m0], n0, true)];
+    for(var ii=i+1; ii<N; ++ii){
+      var m = ii+1;
+      var jn = self.get_jn(m2arr_n[m], n0);
+      if(jn !== -1){
+        var arr_n0 = m2arr_n[m0];
+        var len_j0 = arr_n0.length;
+        var arr_n = m2arr_n[m];
+        var len_j = arr_n.length;
+        var aA = m2A[m];
+        var w = aA[jn]/num_pivot;
+        for(var j0=0; j0<len_j0; ++j0){
+          var aA0j0 = aA0[j0]*w;
+          var nn0 = arr_n0[j0];
+          var jnn = self.get_jn(m2arr_n[m], nn0);
+          if(jnn === -1){
+            arr_n.push(nn0);
+            aA.push(-aA0j0);
+          }
+          else{
+            aA[jnn] -= aA0j0;
+          }
+        }
+        b[ii] -= b[i]*w;
+      }
+    }
+  }
+  return self;
+};
+My_entry.solver_real.prototype.gaussian_coo_backward = function(options, obj_Axb){
+  var self = this;
+  var N = obj_Axb.N;
+  var m2arr_n = obj_Axb.m2arr_n;
+  var m2A = obj_Axb.m2A;
+  var b = obj_Axb.b;
+  var x = obj_Axb.x;
+  if(N > 0){
+    var i = N-1;
+    var m0 = i+1;
+    var n0 = i+1;
+    var nume = b[i];
+    var deno = m2A[m0][self.get_jn(m2arr_n[m0], n0, true)];
+    x[i] = nume/deno;
+  }
+  if(N > 1){
+    for(var i=N-2; i>=0; --i){
+      var m0 = i+1;
+      var n0 = i+1;
+      var arr_n0 = m2arr_n[m0];
+      var len_j0 = arr_n0.length;
+      var aA0 = m2A[m0];
+      var nume = b[i];
+      for(var j0=0; j0<len_j0; ++j0){
+        var nn0 = arr_n0[j0];
+        if(nn0 > n0){
+          nume -= aA0[self.get_jn(m2arr_n[m0], nn0, true)]*x[nn0-1];
+        }
+      }
+      var deno = aA0[self.get_jn(m2arr_n[m0], n0, true)];
+      x[i] = nume/deno;
+    }
+  }
+  return self;
+};
---
+++ 03_solver/js/03_solver.js
@@ -0,0 +1,113 @@
+// online-simulator.github.io
+
+My_entry.test_solver = function(){
+  var self = this;
+  self.init.apply(self, arguments);
+  return self;
+};
+
+My_entry.def.mix_in(My_entry.test_solver, My_entry.original_main);
+
+My_entry.test_solver.prototype.init = function(){
+  var self = this;
+  self.init_main.call(self, ["$"]);
+  return self;
+};
+My_entry.test_solver.prototype.init_elems = function(){
+  var self = this;
+  var $ = self.entry.$;
+  self.elem_o = $._id("textarea-output");
+  $.setup_elems$_tag("button", self.handlers, "onclick");
+  $.setup_elems$_tag("select", self.handlers, "onchange");
+  return self;
+};
+My_entry.test_solver.prototype.init_handlers = function(){
+  var self = this;
+  var $ = self.entry.$;
+  self.handlers.onload = function(e){
+    var self = this;
+    return self;
+  };
+  self.handlers.onbeforeunload = function(e){
+    var self = this;
+    return self;
+  };
+  self.handlers.onclick = function(e, elem){
+    var self = this;
+    var check_num = function(arr){
+      var len_j = arr.length;
+      for(var j=0; j<len_j; ++j){
+        var num = arr[j];
+        if(isNaN(num)){
+          throw "element isNaN";
+        }
+        else{
+          arr[j] = Number(num);
+        }
+      }
+    };
+    switch(elem.id){
+      case "Gauss":
+        try{
+          var solver = new My_entry.solver_real();
+          var text_A = $._id("input-A").value;
+          var text_b = $._id("input-b").value;
+          var A = text_A.split(":");
+          var b = text_b.split(",");
+          check_num(b);
+          var len_i = A.length;
+          var len_b = b.length;
+          if(len_i !== len_b) throw "Invalid Ab size";
+          for(var i=0; i<len_i; ++i){
+            A[i] = A[i].split(",");
+            var len_j = A[i].length;
+            if(len_i !== len_j) throw "Invalid A size";
+            check_num(A[i]);
+          }
+          var obj = {A: A, b: b, x: []};
+          solver.gaussian({}, obj);
+          self.elem_o.value = obj.x;
+        }
+        catch(e){
+          self.elem_o.value = e;
+        }
+        break;
+      case "Gauss_coo":
+        try{
+          var solver = new My_entry.solver_real();
+          var text_b = $._id("input-b").value;
+          var text_aA = $._id("input-aA").value;
+          var text_mA = $._id("input-mA").value;
+          var text_nA = $._id("input-nA").value;
+          var b = text_b.split(",");
+          var aA = text_aA.split(",");
+          var mA = text_mA.split(",");
+          var nA = text_nA.split(",");
+          check_num(b);
+          check_num(aA);
+          check_num(mA);
+          check_num(nA);
+          var obj = {aA: aA, mA: mA, nA: nA, b: b, x: []};
+          solver.gaussian_coo({}, obj);
+          self.elem_o.value = obj.x;
+        }
+        catch(e){
+          self.elem_o.value = e;
+        }
+        break;
+      default:
+        break;
+    }
+    return self;
+  };
+  self.handlers.onchange = function(e, elem){
+    var self = this;
+    var id = elem.id;
+    switch(id){
+      default:
+        break;
+    }
+    return self;
+  };
+  return self;
+};
