28354aff92645b3b705f462ffdd80c2bab63c03c
online-simulator <76011671+online-simulator@users.noreply.github.com>
2023-06-05 11:57:28 +0900

    entry 6.219.97.12.51+

--- 00_common/js/02_handler/02_handler_baseview.js
+++ 00_common/js/02_handler/02_handler_baseview.js
@@ -18,20 +18,20 @@ My_entry.handler_baseview = function(arr_prop_baseview){
   var n = 3;
   var i0 = 0;
   var isLE = true;
-  var val_in = -8388608;
+  var val_set = -8388608;
   var view = self.arr_view[n];
-  self.setInt8n(view, n, i0, val_in, isLE);
-  var val_out = self.getInt8n(view, n, i0, isLE);
+  self.setInt8n(view, n, i0, val_set, isLE);
+  var val_get = self.getInt8n(view, n, i0, isLE);
 
   var n = 3;
   var i0 = 0;
-  var isLE = true;
-  var val_in = 8388608*2-1;
+  var isLE = false;
+  var val_set = 8388608*2-1;
   var view = self.arr_view[n];
-  self.setUint8n(view, n, i0, val_in, isLE);
-  var val_out = self.getUint8n(view, n, i0, isLE);
+  self.setUint8n(view, n, i0, val_set, isLE);
+  var val_get = self.getUint8n(view, n, i0, isLE);
 */
-My_entry.handler_baseview.prototype.setInt8n = function(view, nByte, i0, val10, isLE){
+My_entry.handler_baseview.prototype.setInt8n = function(view, nByte, iByteOffset, val10, isLE){
   var self = this;
   var offset = self.offset[nByte];
   var val16 = parseInt(val10+offset).toString(16);  // val16 >= 0
@@ -42,30 +42,29 @@ My_entry.handler_baseview.prototype.setInt8n = function(view, nByte, i0, val10, 
     var val16i = val16.substring(ie-2, ie) || "0";
     var val10i = Number("0x"+val16i);  // 0~255
     val10i -= (isMSB)? 128: ((val10i < 128)? 0: 256);  // 0x80=128
-    view.setInt8(i0+((isLE)? i: nByte-1-i), val10i);  // -128~127
+    view.setInt8(iByteOffset+((isLE)? i: nByte-1-i), val10i);  // -128~127
   }
   return self;
 };
-My_entry.handler_baseview.prototype.setUint8n = function(view, nByte, i0, val10, isLE){
+My_entry.handler_baseview.prototype.setUint8n = function(view, nByte, iByteOffset, val10, isLE){
   var self = this;
   var offset = 0;
   var val16 = parseInt(val10+offset).toString(16);  // val16 >= 0
   var len16 = val16.length;
   for(var i=0; i<nByte; ++i){
-    var isMSB = (i === nByte-1);
     var ie = len16-i*2;
     var val16i = val16.substring(ie-2, ie) || "0";
     var val10i = Number("0x"+val16i);  // 0~255
-    view.setUint8(i0+((isLE)? i: nByte-1-i), val10i);  // 0~255
+    view.setUint8(iByteOffset+((isLE)? i: nByte-1-i), val10i);  // 0~255
   }
   return self;
 };
-My_entry.handler_baseview.prototype.getInt8n = function(view, nByte, i0, isLE){
+My_entry.handler_baseview.prototype.getInt8n = function(view, nByte, iByteOffset, isLE){
   var self = this;
   var offset = self.offset[nByte];
   var val16 = "";
   for(var i=0; i<nByte; ++i){
-    var val10i = view.getUint8(i0+((isLE)? i: nByte-1-i));
+    var val10i = view.getUint8(iByteOffset+((isLE)? i: nByte-1-i));
     var val16i = "0"+parseInt(val10i).toString(16);  // val16i >= 0
     var ie = val16i.length;
     val16i = val16i.substring(ie-2, ie);  // 0x00~ff
@@ -75,12 +74,12 @@ My_entry.handler_baseview.prototype.getInt8n = function(view, nByte, i0, isLE){
   var _val10 = (val10 < offset)? val10: val10-offset*2;
   return _val10;
 };
-My_entry.handler_baseview.prototype.getUint8n = function(view, nByte, i0, isLE){
+My_entry.handler_baseview.prototype.getUint8n = function(view, nByte, iByteOffset, isLE){
   var self = this;
   var offset = 0;
   var val16 = "";
   for(var i=0; i<nByte; ++i){
-    var val10i = view.getUint8(i0+((isLE)? i: nByte-1-i));
+    var val10i = view.getUint8(iByteOffset+((isLE)? i: nByte-1-i));
     var val16i = "0"+parseInt(val10i).toString(16);  // val16i >= 0
     var ie = val16i.length;
     val16i = val16i.substring(ie-2, ie);  // 0x00~ff
