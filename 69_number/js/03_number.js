e1fc7352af4e18892679173dff25ee4e0ba833f1
online-simulator <76011671+online-simulator@users.noreply.github.com>
2023-07-06 16:31:28 +0900

    entry 6.246.105.13.56

--- 69_number/js/03_number.js
+++ 69_number/js/03_number.js
@@ -20,6 +20,7 @@ My_entry.test_number.prototype.init = function(){
     function(val){
       return "not supported";
     };
+  self.len_m = 12;  // Ver.0.33.4
   return self;
 };
 My_entry.test_number.prototype.init_elems = function(){
@@ -45,6 +46,7 @@ My_entry.test_number.prototype.init_handlers = function(){
     switch(id){
       case "solve0":  // Ver.0.32.4
       case "solve1":  // Ver.0.32.4
+      case "solve2":  // Ver.0.33.4
       case "clear":
       case "convert":
         self[id]();
@@ -100,6 +102,22 @@ My_entry.test_number.prototype.convert = function(){
   return self;
 };
 /* Ver.0.32.4 -> */
+/* Ver.0.33.4 */
+My_entry.test_number.prototype.output_line = function(){
+  var self = this;
+  var _html = "";
+  var hasClass = arguments[0];
+  _html += "<tr";
+  _html += (hasClass)? " class="+hasClass: "";
+  _html += ">";
+  for(var i=1, len=arguments.length; i<len; ++i){
+    _html += "<td>";
+    _html += arguments[i];
+    _html += "</td>";
+  }
+  _html += "</tr>";
+  return _html;
+};
 My_entry.test_number.prototype.solve = function(isMinus){
   var self = this;
   var $ = self.entry.$;
@@ -127,19 +145,9 @@ My_entry.test_number.prototype.solve = function(isMinus){
       }
     }
   };
-  var output = function(){
-    var _html = "";
-    var hasClass = arguments[0];
-    _html += "<tr";
-    _html += (hasClass)? " class="+hasClass: "";
-    _html += ">";
-    for(var i=1, len=arguments.length; i<len; ++i){
-      _html += "<td>";
-      _html += arguments[i];
-      _html += "</td>";
-    }
-    _html += "</tr>";
-    return _html;
+  /* Ver.0.33.4 */
+  var get_Nrshift = function(i){
+    return (-1+(i+1)/freq2d[1][0]);
   };
   var calc_Nrshift = (isMinus)?
     function(i){
@@ -189,10 +197,10 @@ My_entry.test_number.prototype.solve = function(isMinus){
     };
   var run = function(num, N){
     var _html = "";
-    _html += output("", "No.", "dec", "hex", "oct", "bin", "frequency ls3bit=001,011,101,111", "<a title='Graphing Calculator' href='../02_calc_graphing/index.html'>Nrshift</a>");
+    _html += self.output_line("wF", "No.<br>&lt;1e6", "dec", "hex", "oct", "bin", "frequency distribution<br>ls3bit=001,011,101,111", "Nrshift(3)", "-1+(No.+1)/sum(frequency)");  // Ver.0.33.4
     for(var i=0; i<N; ++i){
       if(num%2n === 0n){
-        _html += output("", i, num, num.toString(16), num.toString(8), num.toString(2));
+        _html += self.output_line("", i, num, num.toString(16), num.toString(8), num.toString(2));  // Ver.0.33.4
         if(isBreak(num, i)){
           break;
         }
@@ -200,7 +208,7 @@ My_entry.test_number.prototype.solve = function(isMinus){
       }
       else{
         save_freq(num);
-        _html += output("condition", i, num, num.toString(16), num.toString(8), num.toString(2), freq2d[3], calc_Nrshift(i));
+        _html += self.output_line("condition", i, num, num.toString(16), num.toString(8), num.toString(2), freq2d[3], calc_Nrshift(i), get_Nrshift(i));  // Ver.0.33.4
         if(isBreak(num, i)){
           break;
         }
@@ -220,6 +228,54 @@ My_entry.test_number.prototype.solve0 = function(){
 My_entry.test_number.prototype.solve1 = function(){
   var self = this;
   self.solve(true);
+  return self;
+};
+/* Ver.0.33.4 */
+My_entry.test_number.prototype.solve2 = function(){
+  var self = this;
+  var $ = self.entry.$;
+  self.convert();
+  var count_N0 = function(bin, m){
+    var _N0 = 0;
+    var len = bin.length;
+    for(var i=len-1; i>=len-m; --i){
+      if(bin.charAt(i) === "0"){
+        _N0++;
+      }
+      else{
+        break;
+      }
+    }
+    return _N0;
+  };
+  var get_lsmbit = function(bin, m){
+    var len = bin.length;
+    return bin.substring(len-m, len);
+  };
+  var run = function(){
+    var header0 = self.output_line("wF", "m", "3*n-1", "3*n+1");
+    var header1 = self.output_line("wF", "m", "n", "3*n-1", "3*n+1");
+    var html0 = "";
+    var html1 = "";
+    var len_m = self.len_m;
+    for(var m=1; m<len_m; ++m){
+      var len_n = Math.pow(2, m);
+      var len_n2 = len_n/2;
+      var N0m = 0;
+      var N0p = 0;
+      for(var n=1; n<len_n; n+=2){
+        var binm = (3*n-1).toString(2);
+        var binp = (3*n+1).toString(2);
+        N0m += count_N0(binm, m);
+        N0p += count_N0(binp, m);
+        html1 += self.output_line((m%2 === 1)? "condition": "wF", m, n.toString(2), get_lsmbit(binm, m), get_lsmbit(binp, m));
+      }
+      html0 += self.output_line("", m, N0m+"/"+len_n2, N0p+"/"+len_n2);
+    }
+    $._id("output-log").innerHTML = "<caption class='condition'>simple expected value<br>Nrshift(m) with zeros counted<br>from bit string</caption>"+header0+html0;
+    $._id("output-html").innerHTML = "<caption class='condition'>bit string</caption>"+header1+html1;
+  };
+  run();
   return self;
 };
 /* -> Ver.0.32.4 */
