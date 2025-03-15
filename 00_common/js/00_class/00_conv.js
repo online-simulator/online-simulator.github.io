b5a35be768409b8253a2545a3adf9549beb38634
online-simulator <76011671+online-simulator@users.noreply.github.com>
2025-03-16 04:28:18 +0900

    entry 6.467.607.22.134_2

--- 00_common/js/00_class/00_conv.js
+++ 00_common/js/00_class/00_conv.js
@@ -32,7 +32,7 @@ My_entry.conv.prototype.dec2round_sw = function(dec, sw, opt_digit){
   var self = this;
   var _dec = Number(dec);
   var digit = Number(opt_digit || 1);
-  var mc = (isNaN(_dec))? null: _dec.toExponential(digit+1).split("e");
+  var mc = (_dec%1 === 0 || isNaN(_dec))? null: _dec.toExponential(digit+1).split("e");  // Ver.2.770.117
   if(mc && mc.length === 2){
     _dec = Math[sw || "floor"](Number(mc[0])*Math.pow(10, digit))*Math.pow(10, Number(mc[1])-digit);
   }
