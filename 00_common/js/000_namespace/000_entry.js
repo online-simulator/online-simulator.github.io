8b31aac91d4e2245be57202743a54e5e1ea762cc
online-simulator <76011671+online-simulator@users.noreply.github.com>
2026-05-31 01:56:24 +0900

    entry 6.512.709.26.188

--- 00_common/js/000_namespace/000_entry.js
+++ 00_common/js/000_namespace/000_entry.js
@@ -5,13 +5,13 @@
 // ECMAScript6
 const My_entry = {
   // major update . minor update . minor change . fatal bug-fix . minor bug-fix
-  VERSION: "online-simulator.github.io.6.512.709.26.187",
+  VERSION: "online-simulator.github.io.6.512.709.26.188",
   // major update. minor update/change. fatal/minor bug-fix
   Ver: {
     // inner
     plot: "2.72.17",
     // outer
-    calc: "2.890.169",
+    calc: "2.890.170",
     fluid: "1.61.2",
     cell: "0.14.4",
     wave: "1.85.16",
