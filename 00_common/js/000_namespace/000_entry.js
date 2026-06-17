1fde7e8c1ab9d5fc1aca386a65b6999bbd308f52
online-simulator <76011671+online-simulator@users.noreply.github.com>
2026-06-18 01:18:57 +0900

    entry 6.524.720.29.198

--- 00_common/js/000_namespace/000_entry.js
+++ 00_common/js/000_namespace/000_entry.js
@@ -5,7 +5,7 @@
 // ECMAScript6
 const My_entry = {
   // major update . minor update . minor change . fatal bug-fix . minor bug-fix
-  VERSION: "online-simulator.github.io.6.524.719.29.198",
+  VERSION: "online-simulator.github.io.6.524.720.29.198",
   // major update. minor update/change. fatal/minor bug-fix
   Ver: {
     // inner
@@ -16,7 +16,7 @@ const My_entry = {
     cell: "0.14.4",
     wave: "1.96.21",
     pen: "1.92.13",
-    else: "0.82.9"
+    else: "0.83.9"
   },
   flag: {
     useES6: (Number("0b0") === 0 && Number("0o0") === 0),  // calc-Ver.2.147.37
